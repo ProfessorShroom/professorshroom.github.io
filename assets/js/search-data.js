23cc7fc085431d328d43ce8d463f7801e581abc6
ProfessorShroom <charlie@howardgb.com>
2025-07-06 15:27:58 +0000

    Deploying to gh-pages from @ ProfessorShroom/professorshroom.github.io@1e772075e69cff172f45ca4338d5dfd2d1ff66c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "All of my past, future and present projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
