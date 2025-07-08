8ff43f22d2a4e880485a28231815f4065ad516f0
ProfessorShroom <charlie@howardgb.com>
2025-07-08 10:27:30 +0000

    Deploying to gh-pages from @ ProfessorShroom/professorshroom.github.io@cbddc134f3d09fa614d66d0e05a1a430fc6dc665 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,6 +55,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/Renault_Megane/";
             },},{
+        id: 'social-discord',
+        title: 'Discord',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://discord.com/users/133267005619961856", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
